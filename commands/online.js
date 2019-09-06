b55c15b86e7ac16f1752670a1c92248cc065dcdb
LazyyPimp <54757674+LazyyPimp@users.noreply.github.com>
2019-09-06 14:50:02 -0700

    Update online.js

--- commands/online.js
+++ commands/online.js
@@ -6,6 +6,8 @@ module.exports.run = async (bot, message, args) => {
 bot.user.setStatus('online')
   .then(console.log)
   .catch(console.error);
+  
+    message.delete().catch();
 
   (message.channel.send(`Set status to "online"!`).then(msg => msg.delete(5000)));
 
