85fccb8df61950c0b9e4950431d178c597f63d31
LazyyPimp <54757674+LazyyPimp@users.noreply.github.com>
2019-09-06 14:50:20 -0700

    Update dnd.js

--- commands/dnd.js
+++ commands/dnd.js
@@ -6,7 +6,10 @@ module.exports.run = async (bot, message, args) => {
 bot.user.setStatus('dnd')
   .then(console.log)
   .catch(console.error);
+  
+    message.delete().catch();
   (message.channel.send(`Set status to "dnd"!`).then(msg => msg.delete(5000)));
+  
 }
 
 module.exports.help = {
