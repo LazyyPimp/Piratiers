afd29a248649e7404a9396eadfcae005da5c0a2d
LazyyPimp <54757674+LazyyPimp@users.noreply.github.com>
2019-09-07 05:23:55 -0700

    Update invis.js

--- commands/invis.js
+++ commands/invis.js
@@ -3,6 +3,7 @@ const Discord = require("discord.js");
 
 
 module.exports.run = async (bot, message, args) => {
+  if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("You dont have such permission.");
 bot.user.setStatus('invisible')
   .then(console.log)
   .catch(console.error);
