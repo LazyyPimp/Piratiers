94bf354ad33b45486c46408a7ec25eb1adf9ee0a
LazyyPimp <54757674+LazyyPimp@users.noreply.github.com>
2019-09-07 17:09:28 -0700

    Update setGame.js

--- commands/setGame.js
+++ commands/setGame.js
@@ -1,6 +1,8 @@
+const oid = process.env.oid
+
 module.exports.run = (bot, message, args, discord) => {
  var game = args.join(' ')
-  if(!message.member.hasPermission("MANAGE_MESSAGES")) return errors.noPerms(message, "MANAGE_MESSAGES");
+if (message.author.id == oid) {
    if (!game) {
     message.channel.send(`:ok_hand: Okay, I will set my activity back to normal!`)
     bot.user.setActivity(`.help | ${bot.guilds.size} servers`, {type: "WATCHING"})
